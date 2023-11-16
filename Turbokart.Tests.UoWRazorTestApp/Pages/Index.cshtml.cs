90ca51352fbc5d89f7d92a396264b4293d6cd8b5
Mads Mikkel Rasmussen <mara@aspit.dk>
2023-11-16 15:07:02 +0100

    added API test project

---
+++ Turbokart.Tests.UoWRazorTestApp/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using Turbokart.Infrastructure.Persistence.Interfaces;
+
+namespace Turbokart.Tests.UoWRazorTestApp.Pages
+{
+    public class IndexModel: PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IUnitOfWork unitOfWork;
+
+        public IndexModel(ILogger<IndexModel> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IUnitOfWork UnitOfWork => unitOfWork;
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
