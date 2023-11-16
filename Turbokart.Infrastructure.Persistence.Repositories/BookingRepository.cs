e51f8642361f23b77aaba5c7cfb17886175a9a31
Mads Mikkel Rasmussen <mara@aspit.dk>
2023-11-16 14:17:23 +0100

    added BookingRepository

---
+++ Turbokart.Infrastructure.Persistence.Repositories/BookingRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Turbokart.Domain.Entities;
+using Turbokart.Infrastructure.Persistence.Interfaces;
+
+namespace Turbokart.Infrastructure.Persistence.Repositories
+{
+    public class BookingRepository: RepositoryBase<Booking>, IBookingRepository
+    {
+        public BookingRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public IEnumerable<Booking> GetTodaysBookings()
+        {
+            return set.Where(b => b.Start.Date == DateTime.Today).ToList();
+        }
+    }
+}
--- Turbokart.Infrastructure.Persistence.Repositories/RepositoryBase.cs
+++ Turbokart.Infrastructure.Persistence.Repositories/RepositoryBase.cs
@@ -6,9 +6,9 @@ namespace Turbokart.Infrastructure.Persistence.Repositories
 {
     public abstract class RepositoryBase<T>: IRepository<T> where T : class
     {
-        private DbSet<T> set;
+        protected DbSet<T> set;
 
-        public RepositoryBase(DbContext dbContext)
+        protected RepositoryBase(DbContext dbContext)
         {
             this.set = dbContext.Set<T>();
         }
