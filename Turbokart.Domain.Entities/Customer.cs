6d14d05feebcbe887306f44b0e5c70e63dfb3cdd
Mads Mikkel Rasmussen <mara@aspit.dk>
2023-11-16 11:27:17 +0100

    added entity classes

---
+++ Turbokart.Domain.Entities/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace Turbokart.Domain.Entities
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public List<Booking> Bookings { get; set; }
+    }
+}
