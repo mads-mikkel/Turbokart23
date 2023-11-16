d3f212ed5f83e336e5519e720f7ef719fa61e7f9
Mads Mikkel Rasmussen <mara@aspit.dk>
2023-11-16 13:54:18 +0100

    added RepositoryBase

---
+++ Turbokart.Infrastructure.Persistence.Repositories/RepositoryBase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Turbokart.Infrastructure.Persistence.Interfaces;
+
+namespace Turbokart.Infrastructure.Persistence.Repositories
+{
+    public abstract class RepositoryBase<T>: IRepository<T> where T : class
+    {
+        private DbSet<T> set;
+
+        public RepositoryBase(DbContext dbContext)
+        {
+            this.set = dbContext.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return set.ToList();
+        }
+
+        public T GetBy(object id)
+        {
+            return set.Find(id);
+        }
+
+        public void Save(T entity)
+        {
+            set.Add(entity);
+        }
+    }
+}
