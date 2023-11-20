749548a99c3838b35ec9b50c08419c6562b667f2
Mads Mikkel Rasmussen <mara@aspit.dk>
2023-11-20 09:59:10 +0100

    added use case for booking

---
+++ Turbokart.Application.UseCases/BookingUseCase.cs
@@ -0,0 +1,33 @@
+﻿using Turbokart.Application.Interfaces;
+using Turbokart.Domain.Entities;
+using Turbokart.Infrastructure.Persistence.Interfaces;
+
+namespace Turbokart.Application.UseCases
+{
+    public class BookingUseCase: IBookingUseCase
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public BookingUseCase(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public void BookNew(DateTime dateTime, Customer customer)
+        {
+            Booking booking = new Booking() { Start = dateTime};
+
+
+        }
+
+        public IEnumerable<Booking> GetAllBookings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Booking> GetTodaysBookings()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
