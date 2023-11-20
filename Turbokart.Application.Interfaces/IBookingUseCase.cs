bacdd07097ad850e4e06c5c05d106985ecba5d85
Mads Mikkel Rasmussen <mara@aspit.dk>
2023-11-20 09:31:47 +0100

    added application interface

---
+++ Turbokart.Application.Interfaces/IBookingUseCase.cs
@@ -0,0 +1,11 @@
+﻿using Turbokart.Domain.Entities;
+
+namespace Turbokart.Application.Interfaces
+{
+    public interface IBookingUseCase
+    {
+        void BookNew(DateTime dateTime, Customer customer);
+        IEnumerable<Booking> GetAllBookings();
+        IEnumerable<Booking> GetTodaysBookings();
+    }
+}
